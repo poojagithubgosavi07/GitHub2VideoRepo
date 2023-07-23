9c166f2d8f620f5403653fb7c796e054fee7649d
Pooja <Pooja@LAPTOP-QNMGACMU>
2023-07-23 13:28:06 +0530

    First Commit

---
+++ GitHub2VideoL/src/main/java/com/pages/LoginPage.java
@@ -0,0 +1,10 @@
+package com.pages;
+
+public class LoginPage {
+
+	public void loginpage()
+	{
+		System.out.println("This is loginpage");
+		System.out.println("This is change/update commit");
+	}
+}
